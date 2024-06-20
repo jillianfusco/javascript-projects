7e5f96938c11bcf25a06662eb8765fa399c9974b
Jillian Fusco <fuscojillian@gmail.com>
2024-06-20 12:05:36 -0400

    commit exercises

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,18 @@
-let num = 1001;
+let num = 12345;
 
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+let numLength = num.toString().length
+console.log(numLength)
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+if (String(num).includes('.')) {
+    console.log(String(num).length-1);
+} 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num).includes('.')) {
+    console.log(String(num).length-1);
+} else {
+    console.log(String(num).length);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,36 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1) + language.slice(4,5));
+
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+let result = language[0] + language[4]; 
+console.log(result)
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbreviation for JavaScript is ${result}`)
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+let fun = language.charAt(0).concat(language.charAt(4)).toLowerCase();
 
+console.log(`fun = ${fun}`)
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let titleCaseUpper = notTitleCase.toUpperCase()
+console.log(titleCaseUpper)
+let character1 = titleCaseUpper.charAt(0)
+let character2 = titleCaseUpper.charAt(6)
+console.log(character1 + character2)
+let titleCase = character1 
+                + notTitleCase.charAt(1) 
+                + notTitleCase.charAt(2)
+                + notTitleCase.charAt(3)
+                + notTitleCase.charAt(4)
+                + notTitleCase.charAt(5)
+                + character2
+                + notTitleCase.charAt(7)
+                + notTitleCase.charAt(8)
+                + notTitleCase.charAt(9)
+console.log(titleCase)
