3ac690e2d01419c6a3c0855ac8b8d69f441a18c2
Jillian Fusco <fuscojillian@gmail.com>
2024-07-09 17:00:45 -0400

    commit

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -12,10 +12,32 @@ function getValidInput(prompt, isValid) {
     return userInput;
 }
 
+// let isEven = function(n) {
+//   return Number(n) % 2 === 0;
+// };
+
+// console.log(getValidInput('Enter an even number:', isEven));
+
 // TODO 1: write a validator 
 // that ensures input starts with "a"
+
+let startsWithA = function(word) {
+  return word[0].toLowerCase() === "a"
+}
+
+// console.log(getValidInput(`Enter a word that starts with "A": `, startsWithA));
 
 // TODO 2: write a validator 
 // that ensures input is a vowel
 
+let isAVowel = function(word) {
+  return word.toLowerCase() === "a" 
+  || word.toLowerCase() === "e" 
+  || word.toLowerCase() === "i" 
+  || word.toLowerCase() === "o" 
+  || word.toLowerCase() === "u" 
+  || word.toLowerCase() === "y"; 
+}
+
+console.log(getValidInput(`Enter a vowel: `, isAVowel));
 // Be sure to test your code!
