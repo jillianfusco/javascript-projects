01633aeb11bb16afd76f5ecae6bbfa8c64802cba
Jillian Fusco <fuscojillian@gmail.com>
2024-07-19 18:05:54 -0400

    exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -4,6 +4,13 @@ function gradeLabs(labs) {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+try {
+  result = lab.runLab(3);
+} catch (error) {
+  result = "Error thrown";
+
+}
+  
 }
 
 let studentLabs = [
@@ -21,4 +28,26 @@ let studentLabs = [
   }
 ];
 
-gradeLabs(studentLabs);
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
+
