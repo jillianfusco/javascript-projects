db9765bbceb42827219fead0f5270bea680fc298
Jillian Fusco <fuscojillian@gmail.com>
2024-07-23 19:20:57 -0400

    examples

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -1,9 +1,10 @@
 //Try adding new properties inside constructor.
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name, age, mass, color){
      this.name = name;
      this.age = age;
      this.mass = mass;
+     this.color = color;
    }
 }
 
@@ -12,6 +13,7 @@ let fox = new Astronaut('Fox', 7, 12);
 console.log(fox);
 console.log(fox.age, fox.color);
 
+
 fox.age = 9;
 fox.color = 'red';
 
@@ -19,3 +21,6 @@ console.log(fox);
 console.log(fox.age, fox.color);
 
 //Try modifying or adding properties below.
+
+let panda = new Astronaut("yinyang", 100, 50, "black and white");
+console.log(panda);
