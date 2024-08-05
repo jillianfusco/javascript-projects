48833422715ddfcdab9bd664ed247dc8f5841de8
Jillian Fusco <fuscojillian@gmail.com>
2024-08-05 15:06:16 -0400

    exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,30 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
+    
     // Put your code for the exercises here.
     
+    // liftoff on button
+    button.addEventListener("click", function() {   
+    paragraph.innerHTML = "<br>Houston, we have liftoff!"
+    console.log("liftoff button clicked")
+    });
+
+    // abort turns red
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "#E4002B";
+    });
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    });
+    
+    // confirmation window
+    missionAbort.addEventListener("click", function() {
+        const userConfirmed = window.confirm("Are you sure you want to abort the mission?")
+        if (userConfirmed) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    });
 }
 
 window.addEventListener("load", init);
